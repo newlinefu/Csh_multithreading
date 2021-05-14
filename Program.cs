f8ac0dd13947debb0adea2336695b65f01e1dca3
Alexandr Smirnov <newlinefu@gmail.com>
2021-05-14 09:34:20 +0300

    Task 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
 
 namespace Multithreading
 {
@@ -6,7 +10,30 @@ namespace Multithreading
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            UserManager um = new UserManager();
+            var users = um.InitializeAll(150);
+
+            List<User> firstPart = users.Take(50).ToList();
+            List<User> secondPart = users.Skip(50).Take(50).ToList();
+            List<User> thirdPart = users.Skip(100).Take(50).ToList();
+            
+            Thread firstTr = new Thread(() => um.DoWork(firstPart));
+            Thread secondTr = new Thread(() => um.DoWork(secondPart));
+            Thread thirdTr = new Thread(() => um.DoWork(thirdPart));
+            
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            
+            firstTr.Start();
+            secondTr.Start();
+            thirdTr.Start();
+
+            firstTr.Join();
+            secondTr.Join();
+            thirdTr.Join();
+            
+            Console.WriteLine(sw.Elapsed);
+            sw.Reset();
         }
     }
 }
---
+++ User.cs
@@ -0,0 +1,23 @@
+﻿namespace Multithreading
+{
+    public class User
+    {
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+            set { _name = value; }
+        }
+
+        public User(string name)
+        {
+            _name = name;
+        }
+
+        public User() : this(null)
+        {
+            
+        }
+    }
+}
