a93ebda99d4d9f0e0d23c5aba966e6f24341dac5
Alexandr Smirnov <newlinefu@gmail.com>
2021-05-14 10:17:51 +0300

    Task 6 corrected

--- Program.cs
+++ Program.cs
@@ -17,39 +17,76 @@ namespace Multithreading
             int counter = 0;
             Stopwatch sw = new Stopwatch();
             
+            List<User> firstPart = users.Take(500).ToList();
+            List<User> secondPart = users.Skip(500).Take(500).ToList();
+            List<User> thirdPart = users.Skip(1000).Take(500).ToList();
             
+            Thread firstTr = new Thread(() => um.DoWork(firstPart, CancellationToken.None));
+            Thread secondTr = new Thread(() => um.DoWork(secondPart, CancellationToken.None));
+            Thread thirdTr = new Thread(() => um.DoWork(thirdPart, CancellationToken.None));
+
             sw.Start();
-            um.DoWork(users, CancellationToken.None); // 00:00:23.3498619
+            
+            #region Sync Test
+            
+            um.DoWork(users, CancellationToken.None); // 00:00:23.4344743
             Console.WriteLine($"\nSYNC TIME: {sw.Elapsed}\n");
-            sw.Restart();
             
-            sw.Start();
-            Thread t = new Thread(() => um.DoWork(users, CancellationToken.None)); // 00:00:23.4280668
-            t.Start();
-            t.Join();
-            Console.WriteLine($"\nTHREAD TIME: {sw.Elapsed}\n");
-            sw.Restart();
+            #endregion
             
-            sw.Start();
-            um.DoWorkParallel(users, CancellationToken.None); // 00:00:02.7757682
-            Console.WriteLine($"\nPARALLEL FOREACH TIME: {sw.Elapsed}\n");
             sw.Restart();
+
+            #region Thread test
+
+            firstTr.Start();
+            secondTr.Start();
+            thirdTr.Start();
             
-            sw.Start();
+            firstTr.Join();
+            secondTr.Join();
+            thirdTr.Join();
+            Console.WriteLine($"\nTHREAD TIME: {sw.Elapsed}\n"); // 00:00:07.8620426
+
+            #endregion
+            
+            sw.Restart();
+
+            #region Parallel method test
+
+            um.DoWorkParallel(users, CancellationToken.None); // 00:00:03.2248416
+            Console.WriteLine($"\nPARALLEL METHOD TIME: {sw.Elapsed}\n");
+
+            #endregion
+           
+            sw.Restart();
+
+            #region Parallel invoke test
+            
             Parallel.Invoke(() =>
             {
-                um.DoWork(users, CancellationToken.None);
+                um.DoWork(firstPart, CancellationToken.None);
                 Interlocked.Increment(ref counter);
-            }); // 00:00:23.3873979
+            }, () =>
+            {
+                um.DoWork(secondPart, CancellationToken.None);
+                Interlocked.Increment(ref counter);
+            }, () =>
+            {
+                um.DoWork(thirdPart, CancellationToken.None);
+                Interlocked.Increment(ref counter);
+            });  // 00:00:07.8507226
+            
             while (true)
             {
-                if (counter == 1)
+                if (counter == 3)
                 {
                     Console.WriteLine($"\nPARALLEL FOREACH TIME: {sw.Elapsed}\n");
                     sw.Stop();
                     break;
                 }
             }
+            
+            #endregion
         }
     }
 }
